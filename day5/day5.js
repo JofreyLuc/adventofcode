9b04749f5dc221612a1f4ce0f434d58bbad7ea88
Jofrey Luc <jofrey.luc@up.coop>
2023-12-05 16:40:25 +0100

    day 5_2

--- day5/day5.js
+++ day5/day5.js
@@ -1,27 +1,65 @@
 const fs = require('fs');
 
-applyRuleToData = (rule, value) => {
-  const ruleParams = rule.split(' ').map(Number);
-  if (value < ruleParams[1] || value > ruleParams[1] + ruleParams[2]) {
-    return value;
+applyRulesToData = (rules, value) => {
+  //console.log(rule, value);
+
+  const parsedRules = rules.map(rule => rule.split(' ').map(Number));
+  //console.log(parsedRules);
+
+  const goodRule = parsedRules.find(rule => {
+    return !(value < rule[1] || value >= rule[1] + rule[2]);
+  })
+
+  if (goodRule) {
+    return goodRule[0] + (value - goodRule[1]);
   }
 
-  return ruleParams[0] + (value - ruleParams[1]);
+  return value;
 }
 
 fs.readFile('day5/day5.txt', 'utf8', (err, data) => {
   const steps = data
     .replaceAll(/.*:/g, '*')
     .replaceAll('*\n', '*')
-
+    .replaceAll(/\n\s*\n/g, '\n')
     .split('*');
-  console.log(steps);
+  steps.shift();
+
+  //steps.forEach(console.log);
+
   let values = steps.shift().trim().split(' ').map(Number);
 
-  steps.forEach(step => {
-    const rules = step.split('\n');
-    values = values.map(seed => rules.reduce((newSeed, currentRule) => applyRuleToData(currentRule, newSeed), seed));
-  });
+  console.log('value pairs : ' + values.length / 2);
 
-  console.log(values.reduce((min, value) => Math.min(min, value), Number.MAX_VALUE));
+  const mins = [];
+  for (let i = 0; i < values.length; i += 2) {
+    console.log('pair ' + i/2);
+    const num = values[i];
+    const range = values[i + 1];
+    console.log('num : ' + num);
+    console.log('range : ' + range)
+    let min = Number.MAX_VALUE;
+    for (let currentValue = num; currentValue < num + range; currentValue++) {
+      steps.forEach(step => {
+        const rules = step.split('\n');
+        rules.pop();
+        //console.log(values);
+        min = Math.min(min, applyRulesToData(rules, currentValue));
+      });
+    }
+
+    /*steps.forEach(step => {
+      const rules = step.split('\n');
+      rules.pop();
+      //console.log(values);
+      currentValues = currentValues.map(seed => applyRulesToData(rules, seed));
+    });*/
+
+    mins.push(min);
+  }
+
+
+
+
+  console.log(mins.reduce((min, value) => Math.min(min, value), Number.MAX_VALUE));
 });
